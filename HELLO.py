9a680e57c3c4c5d321cef0ebdb4dc1c8bb91bbc7
KARTHIK11-05 <146980801+KARTHIK11-05@users.noreply.github.com>
2023-10-09 23:00:13 +0530

    AREA_OF__CIRCLE.py

--- HELLO.py
+++ HELLO.py
@@ -2,4 +2,5 @@ R=int(input('ENTER THE RADIUS OF CIRLCLE'))
 PI=22/7
 AREA = PI*R*R
 print( 'AREA OF CIRCLE IS =' + str(AREA) +' Sq Units')
-print('Nice you have done It')
+print('Nice you have done It') 
+
